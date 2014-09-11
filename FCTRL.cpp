a3e6ab46e22fa1cd2aebcdacd40210f2579109ab
Rohan Phadke <rohanphadke@users.noreply.github.com>
2014-09-11 16:20:49 +0530

    Create FCTRL.cpp
    SPOJ 11 - Factorial
    (Easy)

---
+++ FCTRL.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+#define gc getchar
+#define MOD 100000007
+#define pc(x) putchar(x);
+#define pi(n) printf("%d",n)
+#define pll(n) printf("%lld",n)
+#define ps printf(" ")
+#define pn printf("\n")
+#define rep(i,n) for(i=0;i<n;i++)
+#define fu(i,a,n) for(i=a;i<=n;i++)
+#define fd(i,n,a) for(i=n;i>=a;i--)
+#define INV 333333336
+#define ll long long
+#define EPS 1e-9
+#define infi 2000000000
+
+int ctoi(){
+	char c;
+	while(c=getchar(),(c<'0'||c>'9')&&(c!='-'));
+	bool flag=(c=='-');
+	if(flag)
+		c=getchar();
+	int x=0;
+	while(c>='0'&&c<='9'){
+		x=x*10+c-48;
+		c=getchar();
+    }
+	return flag?-x:x;
+}
+
+inline int sn()
+{
+    int n=0;
+    int ch=gc();
+    while( ch <48 )ch=gc();
+    while( ch >47 )
+    n = (n<<3)+(n<<1) + ch-'0', ch=gc();
+        return n;
+}
+
+inline void wi(long long n)
+{
+    long long N = n, rev, count = 0;
+    rev = N;
+    if (N == 0) { pc('0');  return ;}
+    while ((rev % 10) == 0) { count++; rev /= 10;} //obtain the count of the number of 0s
+    rev = 0;
+    while (N != 0) { rev = (rev<<3) + (rev<<1) + N % 10; N /= 10;}  //store reverse of N in rev
+    while (rev != 0) { pc(rev % 10 + '0'); rev /= 10;}
+    while (count--) pc('0');
+}
+
+/* Number of trailing zeroes in the factorial of a number is the number of times each power of 5 less than the given number n appears in the factorization of n! (n factorial) */
+
+int main(){
+    int t=ctoi();
+    while(t--){
+        int num=ctoi();
+        long long ans=0;
+        while(num>0){
+            num/=5;
+            ans+=num;
+        }
+        wi(ans);pn;
+    }
+    return 0;
+}
