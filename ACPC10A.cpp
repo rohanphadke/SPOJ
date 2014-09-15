987036dcf29b29fc0ac83bb0b6fb6c2f5f8c7b49
Rohan Phadke <rohanphadke@users.noreply.github.com>
2014-09-15 19:01:35 +0530

    Create ACPC10A.cpp
    SPOJ 7974 - What's Next
    (Very Easy,Ad-Hoc)

---
+++ ACPC10A.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+#define gc getchar
+#define MOD 100000007
+#define pc(x) putchar(x);
+#define pi(n) printf("%d",n)
+#define pll(n) printf("%lld",n)
+#define ps printf(" ")
+#define pn printf("\n")
+#define rep(i,n) for(i=0;i<n;i++)
+#define fu(i,a,n) for(i=a;i<=n;i++)
+#define fd(i,n,a) for(i=n;i>=a;i--)
+#define INV 333333336
+#define ll long long
+#define EPS 1e-9
+#define infi 2000000000
+
+//fast input
+int ctoi(){
+	char c;
+	while(c=getchar(),(c<'0'||c>'9')&&(c!='-'));
+	bool flag=(c=='-');
+	if(flag)
+		c=getchar();
+	int x=0;
+	while(c>='0'&&c<='9'){
+		x=x*10+c-48;
+		c=getchar();
+    }
+	return flag?-x:x;
+}
+
+//fast output
+inline void wi(int n)
+{
+    int N = n, rev, count = 0;
+    rev = N;
+    if (N == 0) { pc('0');  return ;}
+    while ((rev % 10) == 0) { count++; rev /= 10;} //obtain the count of the number of 0s
+    rev = 0;
+    while (N != 0) { rev = (rev<<3) + (rev<<1) + N % 10; N /= 10;}  //store reverse of N in rev
+    while (rev != 0) { pc(rev % 10 + '0'); rev /= 10;}
+    while (count--) pc('0');
+}
+
+int main(){
+    while(1){
+        int a=ctoi(),b=ctoi(),c=ctoi();
+        if(a==0 && b==0 && c==0){
+            break;
+        }
+        if(b-a==c-b){                             //check if AP
+            printf("AP %d\n",2*c-b);
+        }else{                                    //if not AP its bound to be GP
+            printf("GP %d\n",c*c/b);
+        }
+    }
+    return 0;
+}
